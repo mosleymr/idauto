a182f3572399c1502c2d6f79d34645a5b41bc869
mmosley <mosleym@gmail.com>
2025-10-11 01:12:50 -0400

    fixed next.config.ts export

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/idauto',
-  assetPrefix: '/idauto',
   /* config options here */
 };
 
